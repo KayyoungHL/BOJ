0c00da2e7ab6f2acaa893b898c030f8ae238d0d1
KayyoungHL <kayyounghl@gmail.com>
2021-11-17 18:00:12 +0900

    INITIAL COMMIT

--- Appendfirstline.py
+++ Appendfirstline.py
@@ -3,13 +3,13 @@ import re
 
 def main() :
     curr_path = os.path.dirname(os.path.abspath(__file__))
-    file_list = os.listdir(curr_path)
+    file_list = os.listdir(curr_path+"\problem")
     print(curr_path)
     print(file_list)
     for i in file_list :
         if i[-2:] != "py" : 
             continue ################## 새로 넣은 문구. 확인 후 돌릴 것!
-        with open(i, 'r+', encoding='utf-8') as fp :
+        with open("./problem/"+i, 'r+', encoding='utf-8') as fp :
             file_text = fp.read()
             if file_text.startswith("# https://www.acmicpc.net/problem/") : continue
             else :
